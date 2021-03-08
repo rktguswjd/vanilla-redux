f032eccbfd5b31c013a009413dafbda009ca63af
rktguswjd <010_cm@naver.com>
2021-03-08 22:12:07 +0900

    Avoid state mutation

--- src/index.js
+++ src/index.js
@@ -11,20 +11,24 @@ const reducer = (state = [], action) => {
     console.log(action);
     switch (action.type) {
         case ADD_TODO:
-            return [];
+            // mutate state는 절대로 하면 안됨!
+            // 이전 상태로 새로운 객체를 리턴해줘야함 !
+            return [...state, { text: action.text, id: Date.now() }];
         case DELETE_TODO:
             return [];
         default:
             return state;
     }
 };
-const sotre = createStore(reducer);
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 const onSubmit = (e) => {
     e.preventDefault();
     const toDo = input.value;
     input.value = "";
-    sotre.dispatch({ type: ADD_TODO, text: toDo });
+    store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
 form.addEventListener("submit", onSubmit);
